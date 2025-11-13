63d623e0f66f08d8fe4aa19a18a3bcd568f901a2
gopusuryaprakash <116333194+gopusuryaprakash@users.noreply.github.com>
2025-11-13 11:50:21 +0530

    Update main.tf

--- Day1/1c_rg_st_using_locals/main.tf
+++ Day1/1c_rg_st_using_locals/main.tf
@@ -14,5 +14,4 @@ resource "azurerm_storage_account" "main" {
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
-
 }
